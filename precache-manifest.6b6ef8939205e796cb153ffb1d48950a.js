0d6ff90bf324fbadd2ef0eba1b9f4354ed409c88
Developer <developer@Dev.local>
2021-11-25 11:19:56 +0800

    Updates

---
+++ precache-manifest.6b6ef8939205e796cb153ffb1d48950a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b7a172b08da269142e3d4f917ba14e7",
+    "url": "/github-user-finder/index.html"
+  },
+  {
+    "revision": "93192320a6f2e88868b2",
+    "url": "/github-user-finder/static/css/main.ae8ec85a.chunk.css"
+  },
+  {
+    "revision": "15d01be9aa2676e13cdf",
+    "url": "/github-user-finder/static/js/2.d8131e92.chunk.js"
+  },
+  {
+    "revision": "93192320a6f2e88868b2",
+    "url": "/github-user-finder/static/js/main.b36f8800.chunk.js"
+  },
+  {
+    "revision": "cefbf2015fa796dac786",
+    "url": "/github-user-finder/static/js/runtime~main.8f4ea36e.js"
+  }
+]);
